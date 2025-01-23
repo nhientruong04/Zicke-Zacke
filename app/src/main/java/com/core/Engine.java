4bb91f38577a7047580fb14383b048d045e56cb0
dducwsxuaan <buiducxuan07062004@gmail.com>
2025-01-23 15:26:42 +0100

    Modify startScene and playerScene

--- app/src/main/java/com/core/Engine.java
+++ app/src/main/java/com/core/Engine.java
@@ -1,6 +1,7 @@
 package com.core;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 import com.systems.ISystem;
 
@@ -13,6 +14,7 @@ import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -23,7 +25,7 @@ public class Engine {
     public EntityCreator entity_creator;
     public NodeCreator node_creator;
     public AnimationTimer GameLoop;
-    
+
     private Initializer initializer;
     private ArrayList<ISystem> systems;
 
@@ -40,54 +42,101 @@ public class Engine {
         this.systems.add(system);
     }
 
-    public Scene createStartScene(){
-        Pane startRoot = new Pane();
+    public Scene createStartScene() {
+        StackPane root = new StackPane();
+        root.setAlignment(javafx.geometry.Pos.CENTER);
+        GridPane buttonGridPane = new GridPane();
+        buttonGridPane.setVgap(50);
+        buttonGridPane.setAlignment(javafx.geometry.Pos.CENTER);
         Button startButton = new Button("Start");
         startButton.setId("start-button");
-        startButton.getStyleClass().add("button-style");
-        startButton.setLayoutX(450);
-        startButton.setLayoutY(450);
-
-        BackgroundSize backgroundSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true);
-        BackgroundImage backgroundImage = new BackgroundImage(new Image(getClass().getResource("/background/startbackground.png").toExternalForm(), 1000, 700, false, true),
+        startButton.getStyleClass().add("start-exit-button-style");
+        
+        Button exitButton = new Button("Exit");
+        exitButton.setId("exit-button");
+        exitButton.getStyleClass().add("start-exit-button-style");
+        
+    
+        BackgroundSize backgroundSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true,
+                true);
+        BackgroundImage backgroundImage = new BackgroundImage(
+                new Image(getClass().getResource("/background/startbackground.png").toExternalForm(), 1000, 700, false,
+                        true),
                 BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
-        startRoot.setBackground(new Background(backgroundImage));
-        startRoot.getChildren().add(startButton);
-        Scene startScene = new Scene(startRoot, 1000, 700);
+        root.setBackground(new Background(backgroundImage));
+        buttonGridPane.add(startButton, 1, 0);
+        buttonGridPane.add(exitButton, 1, 1);
+        root.getChildren().add(buttonGridPane);
+        buttonGridPane.setTranslateY(100);
+        Scene startScene = new Scene(root, 1000, 700);
         return startScene;
     }
 
-    public Scene createModePlayerScene(){
+    public Scene createModePlayerScene() {
+
+        StackPane root = new StackPane();
+        root.setAlignment(javafx.geometry.Pos.CENTER);
+
         Button twoPlayersButton = new Button("2 Players");
         Button threePlayersButton = new Button("3 Players");
         Button fourPlayersButton = new Button("4 Players");
         Button backButton = new Button("Back");
 
+        // Set IDs and styles for buttons
         twoPlayersButton.setId("player2-button");
-        twoPlayersButton.getStyleClass().add("button-style");
+        twoPlayersButton.getStyleClass().add("player-button-style");
         threePlayersButton.setId("player3-button");
-        threePlayersButton.getStyleClass().add("button-style");
+        threePlayersButton.getStyleClass().add("player-button-style");
         fourPlayersButton.setId("player4-button");
-        fourPlayersButton.getStyleClass().add("button-style");
+        fourPlayersButton.getStyleClass().add("player-button-style");
         backButton.setId("back-button");
-        backButton.getStyleClass().add("button-style");
+        backButton.getStyleClass().add("back-button-style");
 
-        BackgroundSize backgroundSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true);
-        BackgroundImage backgroundImage = new BackgroundImage(new Image(getClass().getResource("/background/startbackground.png").toExternalForm(), 1000, 700, false, true),
+        // Create a GridPane to hold the first three buttons
+        GridPane buttonTable = new GridPane();
+        buttonTable.setId("button-table");
+        buttonTable.setVgap(10); // Horizontal gap between elements
+        buttonTable.setAlignment(javafx.geometry.Pos.CENTER); // Center the GridPane
+
+        // Add buttons to the GridPane
+        buttonTable.add(twoPlayersButton, 1, 0); // Column 1, Row 0
+        buttonTable.add(threePlayersButton, 1, 1); // Column 1, Row 1
+        buttonTable.add(fourPlayersButton, 1, 2); // Column 1, Row 2
+
+        GridPane backButtonTable = new GridPane();
+        backButtonTable.setId("button-table");
+        backButtonTable.setVgap(10); // Horizontal gap between elements
+        backButtonTable.setAlignment(javafx.geometry.Pos.CENTER); // Center the GridPane
+
+        // Add buttons to the GridPane
+        backButtonTable.add(backButton, 1, 0); // Column 1, Row 0
+
+        // Background image settings
+        BackgroundSize backgroundSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true,
+                true);
+        BackgroundImage backgroundImage = new BackgroundImage(
+                new Image(getClass().getResource("/background/startbackground.png").toExternalForm(), 1000, 700, false,
+                        true),
                 BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        root.setBackground(new Background(backgroundImage));
 
-        VBox scene2Root = new VBox(20); // VBox with spacing of 20 between elements
-        scene2Root.setBackground(new Background(backgroundImage));
-        scene2Root.getChildren().addAll(twoPlayersButton, threePlayersButton, fourPlayersButton, backButton);
-        scene2Root.setStyle("-fx-alignment: center;"); // Center everything in VBox
-        Scene playersScene = new Scene(scene2Root, 1000, 700);
+        // VBox as root container
+        VBox buttonBox = new VBox(20); // VBox with spacing of 20 between elements
+        buttonBox.getChildren().addAll(buttonTable, backButtonTable); // Add the button table and the back button
+        root.getChildren().add(buttonBox); // Add the VBox to the root StackPane
+        buttonBox.setTranslateY(100); // Move the VBox down by 100 pixels
+        buttonBox.setAlignment(javafx.geometry.Pos.CENTER); // Center the VBox
+        // Create the scene
+        Scene playersScene = new Scene(root, 1000, 700);
         return playersScene;
     }
 
     public Scene createMap() {
         StackPane root = new StackPane();
-        BackgroundSize backgroundSize = new BackgroundSize(800, 800, false, false, true, true);
-        BackgroundImage backgroundImage = new BackgroundImage(new Image(getClass().getResource("/background/background7.jpg").toExternalForm(), 800, 800, false, true),
+        BackgroundSize backgroundSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true);
+        BackgroundImage backgroundImage = new BackgroundImage(
+                new Image(getClass().getResource("/background/background8.jpg").toExternalForm(), 1000, 700, false,
+                        true),
                 BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
         root.setBackground(new Background(backgroundImage));
         Scene firstScene = new Scene(root, Settings.WIDTH, Settings.HEIGHT);
@@ -106,18 +155,18 @@ public class Engine {
             @Override
             public void handle(long arg0) {
                 outer().update();
-            }      
+            }
         };
 
         this.GameLoop.start();
-	}
+    }
 
     public void stop() {
         this.GameLoop.stop();
     }
 
     private void update() {
-        for (int i=0; i<this.systems.size(); i++) {
+        for (int i = 0; i < this.systems.size(); i++) {
             this.systems.get(i).update();
         }
     }
